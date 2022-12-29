abb80498c3d7f54bb97a5aa36b58e7d8c7acb954
Lee Eunbin <enbin02@gmail.com>
2022-12-30 03:44:31 +0900

    필드 추가

--- HealthCare/models.py
+++ HealthCare/models.py
@@ -4,6 +4,9 @@ class Post(models.Model):
     content1 = models.TextField()
     content2 = models.TextField()
     content3 = models.TextField()
+    content4 = models.TextField()
+    content5 = models.TextField()
+    content6 = models.TextField()
     create_date = models.DateTimeField(auto_now_add=True)
 
     def __str__(self):
