7854f24ff1a1615a6d1d1425cb90ca7c5720fcf0
Lee Eunbin <enbin02@gmail.com>
2022-12-30 01:58:23 +0900

    test

--- HealthCare/serializers.py
+++ HealthCare/serializers.py
@@ -2,6 +2,7 @@ from rest_framework import serializers
 from .models import Post
 
 class PostSerializer(serializers.ModelSerializer):
+ 
     class Meta:
         model = Post
         fields = ('content1', 'content2', 'content3', 'create_date')
