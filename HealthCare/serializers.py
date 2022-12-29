f5723f7e8decad21bc44a44694dc1a4d35345cf6
Lee Eunbin <enbin02@gmail.com>
2022-12-30 01:32:01 +0900

    날짜 수정

--- HealthCare/serializers.py
+++ HealthCare/serializers.py
@@ -4,4 +4,4 @@ from .models import Post
 class PostSerializer(serializers.ModelSerializer):
     class Meta:
         model = Post
-        fields = ('content1', 'content2', 'content3')
+        fields = ('content1', 'content2', 'content3', 'create_date')
